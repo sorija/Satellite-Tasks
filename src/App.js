eba2585d608d497b7d94bd915406fc15b6411f5b
Joan <joan.gawelda@gmail.com>
2019-02-05 13:57:10 +0200

    Add functionality for TasksList Component

--- src/App.js
+++ src/App.js
@@ -6,37 +6,72 @@ import "./styles/App.css";
 const tasks = [
   {
     id: 0,
-    satName: "Bespin",
+    satName: "Alderan",
     start: 4934.539,
     end: 34943.023,
     status: "new"
   },
   {
     id: 1,
-    satName: "Dagobah",
-    start: 924753.543,
-    end: 89420.143,
+    satName: "Bespin",
+    start: 249809.342,
+    end: 98342.247,
     status: "new"
   },
   {
     id: 2,
-    satName: "Hoth",
-    start: 1243.876,
-    end: 91348.471,
+    satName: "Coruscant",
+    start: 23634.453,
+    end: 18374.909,
     status: "new"
   },
   {
     id: 3,
-    satName: "Naboo",
-    start: 98673.342,
-    end: 1224325.023,
+    satName: "Dagobah",
+    start: 69529.348,
+    end: 463290.236,
     status: "new"
   },
   {
     id: 4,
-    satName: "Yavin",
-    start: 12442.564,
+    satName: "Hoth",
+    start: 12843.394,
+    end: 423673.634,
+    status: "new"
+  },
+  {
+    id: 5,
+    satName: "Kashyyyk",
+    start: 56932.893,
+    end: 484893.208,
+    status: "new"
+  },
+  {
+    id: 6,
+    satName: "Naboo",
+    start: 789644.873,
+    end: 125423.026,
+    status: "new"
+  },
+  {
+    id: 7,
+    satName: "Tatooine",
+    start: 356983.348,
     end: 153389.092,
+    status: "new"
+  },
+  {
+    id: 8,
+    satName: "Yavin",
+    start: 934732.342,
+    end: 175834.037,
+    status: "new"
+  },
+  {
+    id: 9,
+    satName: "Wobani",
+    start: 754094.346,
+    end: 5645343.056,
     status: "new"
   }
 ];
--- src/TasksList.js
+++ src/TasksList.js
@@ -1,19 +1,46 @@
 import React from "react";
 import Task from "./Task.js";
 
-class TaskList extends React.Component {
+class TasksList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasksLength: this.props.tasks.length,
+      currentPage: 1
+    };
+  }
+  divideTasks = () => {
+    let startIndex = (this.state.currentPage - 1) * 5;
+    let endIndex = this.state.currentPage * 5;
+    return this.props.tasks.slice(startIndex, endIndex);
+  };
+
   render() {
+    // number of pages is equal to number of tasks divided by limit of tasks per page and rounded up
+    const length = this.state.tasksLength / 5;
+    const pagination = Array.from(
+      { length: Math.ceil(length) },
+      (_, i) => i + 1
+    );
     return (
       <div>
         <ul>
-          {this.props.tasks.map(task => (
+          {this.divideTasks().map(task => (
             <li key={task.id}>
               <Task {...task} />
             </li>
           ))}
         </ul>
+        <footer>
+          Pages:
+          <ul>
+            {pagination.map(page => (
+              <li key={page}>{page}</li>
+            ))}
+          </ul>
+        </footer>
       </div>
     );
   }
 }
-export default TaskList;
+export default TasksList;
